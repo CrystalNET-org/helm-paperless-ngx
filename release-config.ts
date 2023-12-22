ac1ba4930425ed1d876b83a03b40cc73e0d90c40
johndoe <anon@ymous.com>
2023-12-22 21:54:15 +0100

    put templated out readme into charts dir

--- release-config.ts
+++ release-config.ts
@@ -2,7 +2,7 @@ export default {
     skipCommitsWithoutPullRequest: false,
     beforePrepare: async ({ exec, nextVersion }) => {
       await exec(`sed -i "s/^version:.*$/version: ${nextVersion}/g" chart/Chart.yaml`);
-      await exec(`helm-docs --template-files=./README.md.gotmpl --output-file=../README.md chart/`);
+      await exec(`helm-docs --template-files=./README.md.gotmpl --output-file=README.md chart/`);
     },
   }
 //TODO: helm-docs --template-files=./README.md.gotmpl --output-file=../README.md chart/
